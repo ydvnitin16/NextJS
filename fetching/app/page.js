17fb20a2991568bc22856d859e8def201712374f
Nitin Yadav <ydvnitin16@gmail.com>
2025-11-06 17:25:19 +0530

    learn fetching data , sequential & parallel fetching

---
+++ fetching/app/page.js
@@ -0,0 +1,62 @@
+// Sequential loading
+import { Suspense } from 'react';
+
+const getTodos = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+    return await res.json();
+};
+
+const getSingleTodo = async (id) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    return await res.json();
+};
+
+// export default async function Page() {
+//     await new Promise((r) => getTodos(r, 3000));
+
+//     // Get artist information
+//     const todos = await getArtist();
+
+//     return (
+//         <>
+//             <Suspense fallback={<div>Loading...</div>}>
+//                 <h1>{todos[0].title}</h1>
+//             </Suspense>
+
+//             {/* Show fallback UI while the Playlists component is loading */}
+//             <Suspense fallback={<div>Loading...</div>}>
+//                 {/* Pass the artist ID to the Playlists component */}
+//                 <Todo todoId={3} />
+//             </Suspense>
+//         </>
+//     );
+// }
+
+// async function Todo({ todoId }) {
+//       await new Promise((r) => setTimeout(r, 3000))
+
+//     // Use the artist ID to fetch playlists
+//     const todo = await getSingleTodo(todoId);
+
+//     return (
+//         <ul>
+//             <li>{todo.title}</li>
+//         </ul>
+//     );
+// }
+
+export default async function Page() {
+ 
+  // Initiate requests
+  const todos = getTodos()
+  const todo = getSingleTodo(3)
+ 
+  const [res1, res2] = await Promise.all([todos, todo])
+ 
+  return (
+    <>
+      <h1>{res1[0].title}</h1>
+      <h1>{res2.title}</h1>
+    </>
+  )
+}
