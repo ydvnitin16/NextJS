017f78154a3413ad4ad943a45a6609d467af0ea9
Nitin Yadav <ydvnitin16@gmail.com>
2025-11-20 20:50:11 +0530

    Learn Image optimization

---
+++ my-app/next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+            },
+        ],
+        qualities: [20, 50, 75, 100],
+    },
+};
